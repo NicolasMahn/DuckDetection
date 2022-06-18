cbe1644e7ddd6840b531bd32bf6e4f1d5785f5a7
Nicolas Mahn <Nicolas.Mahn@hs-furtwangen.de>
2022-06-18 15:34:13 +0200

    Added a Region of Interest, so only the interesting areas are scanned

--- imageProcessingThread.py
+++ imageProcessingThread.py
@@ -12,7 +12,7 @@ def midpoint(ptA, ptB):
     return (ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5
 
 
-def analyse_image(cnts, filename):
+def analyse_image(cnts, filename, place):
     image = cv2.imread(filename)
 
     # sort the contours from left-to-right and initialize the
@@ -23,64 +23,6 @@ def analyse_image(cnts, filename):
         if cv2.contourArea(c) < 100:
             continue
 
-        # compute the rotated bounding box of the contour
-        orig = image.copy()
-        box = cv2.minAreaRect(c)
-        box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
-        box = np.array(box, dtype="int")
 
-        # order the points in the contour such that they appear
-        # in top-left, top-right, bottom-right, and bottom-left
-        # order, then draw the outline of the rotated bounding
-        # box
-        box = perspective.order_points(box)
-        cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
-        # loop over the original points and draw them
-        for (x, y) in box:
-            cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
-
-        # unpack the ordered bounding box, then compute the midpoint
-        # between the top-left and top-right coordinates, followed by
-        # the midpoint between bottom-left and bottom-right coordinates
-        (tl, tr, br, bl) = box
-        (tltrX, tltrY) = midpoint(tl, tr)
-        (blbrX, blbrY) = midpoint(bl, br)
-        # compute the midpoint between the top-left and top-right points,
-        # followed by the midpoint between the top-righ and bottom-right
-        (tlblX, tlblY) = midpoint(tl, bl)
-        (trbrX, trbrY) = midpoint(tr, br)
-        # draw the midpoints on the image
-        cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
-        cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
-        cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
-        cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
-        # draw lines between the midpoints
-        cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
-                 (255, 0, 255), 2)
-        cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
-                 (255, 0, 255), 2)
-
-    '''contours = imutils.grab_contours(contours)'''
-
-    # sort the contours from left-to-right and initialize the
-    '''# 'pixels per metric' calibration variable
-    (contours, _) = contours.sort_contours(contours)
-    pixelsPerMetric = None
-
-    # loop over the contours individually
-    for c in contours:
-        # if the contour is not sufficiently large, ignore it
-        if cv2.contourArea(c) < 100:
-            continue
-
-        # compute the rotated bounding box of the contour
-        orig = image.copy()
-        box = cv2.minAreaRect(c)
-        box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
-        box = np.array(box, dtype="int")'''
-
-    # color = cv2.bitwise_and(image, image, mask=mask)
-    # color = cv2.resize(color, dimentions, interpolation=cv2.INTER_AREA)
-    # cv2.imshow(name, color)
 
     logging.info("%s has been processed", filename)
